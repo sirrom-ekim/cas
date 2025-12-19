40a81d74efcdcc743f6071798df984194fbd7c4c
Mike Morris <morrisprogramming@gmail.com>
2025-12-19 16:20:22 -0500

    Folder Colors

--- resources/views/meetings.blade.php
+++ resources/views/meetings.blade.php
@@ -2,7 +2,7 @@
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
 
-<iframe src="https://workdrive.zohoexternal.com/embed/eyii9c04139f11b274c0ebf6b00d63cea97c1?hidetheme=true&toolbar=false&layout=list&appearance=dark&themecolor=green" scrolling="no" frameborder="0" allowfullscreen="true" width="100%" height="100%" title="Embed code"></iframe>
+<iframe src="https://workdrive.zohoexternal.com/embed/eyii9c04139f11b274c0ebf6b00d63cea97c1?hidetheme=true&toolbar=false&layout=list&appearance=dark&themecolor=blue" scrolling="no" frameborder="0" allowfullscreen="true" width="100%" height="100%" title="Embed code"></iframe>
 
         </div>
     </div>
